3f439f812790c9827d5fa65d2c6223f12d440a28
Jose Ignacio Palma <jignaciopm13@gmail.com>
2026-01-08 09:29:43 -0400

    feat: add 'enable_plugins_before_start' toggle for resource optimization

---
+++ scripts/enable_inline_tutor_plugins.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+app_name="$1"
+inline_folder="$2"
+
+source .tutor_venv/bin/activate
+cp -r "${app_name}/${inline_folder}"/. plugins/
+
+for plugin_file in plugins/*; do
+  plugin_name="$(basename "$plugin_file" | cut -f1 -d '.')"
+  tutor plugins enable "$plugin_name"
+
