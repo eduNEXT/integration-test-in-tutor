47e066c9fb9ec7424e0552268e313ac532b3ad9a
Jose Ignacio Palma <jignaciopm13@gmail.com>
2026-01-20 18:24:55 -0400

    fix: script unexpected end of file

--- scripts/enable_inline_tutor_plugins.sh
+++ scripts/enable_inline_tutor_plugins.sh
@@ -10,4 +10,4 @@ cp -r "${app_name}/${inline_folder}"/. plugins/
 for plugin_file in plugins/*; do
   plugin_name="$(basename "$plugin_file" | cut -f1 -d '.')"
   tutor plugins enable "$plugin_name"
-
+done
